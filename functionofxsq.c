924f2d7696b8d408c6d61508238886fbc1fb67d9
thisispraphullverma@gmail.com <thisispraphullverma@gmail.com>
2025-01-08 01:29:42 +0530

    Add Y=X2 + 2X + 1 Question

---
+++ functionofxsq.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    /*QUESTION: Write a C program to evaluate the following expression:
+    Y=x2+2x+1 for a given value of x.
+    */
+    int x = 5;
+    int y = (x*x) + (2*x) + 1;
+    printf("The value of y is: %d", y);
+    return 0;
+
+
+
+}
